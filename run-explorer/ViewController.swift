e9aa4e1d61d37434799d8c6ae3ec0463b814ae4f
Konstantin Klitenik <kostyan5@gmail.com>
2019-05-31 14:06:39 -0400

    Initial commit

---
+++ run-explorer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  run-explorer
+//
+//  Created by Konstantin Klitenik on 5/31/19.
+//  Copyright © 2019 KK. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
